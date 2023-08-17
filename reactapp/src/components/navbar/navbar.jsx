1770ab73b994856a255e0283a90960ffc609957d
Jool333 <jool333@live.se>
2023-08-17 12:31:37 +0200

    Added the navbar from previous proj, updated/translated from cshtml to jsx, fixed the img src path for all navbar imgs, added and imported the css for the navbar to the new proj

---
+++ reactapp/src/components/navbar/navbar.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './navbar.css'
+
+var imgSrc = "./src/assets/images/";
+function navbar() {
+  return (
+      <nav className="navbar ">
+          <div className="container-navbar">
+              <div className="navbar-header">
+                  <div className="header-side-items">
+                      <div className="nav-head-left">
+                          <div className="datetime" id="datetime">{new Date().toLocaleDateString("en-GB")}</div>
+                          <div id="weather" className="weather-button-container">
+                              <button id="weatherButton">
+                                  <img src={imgSrc + "weather/clear_day.svg"} alt="WeatherImg not found" id="condition" />
+                                  <div id="temperature">Temp:</div>
+                              </button>
+                          </div>
+                      </div>
+                      <div className="nav-head-center">
+                          <img src={imgSrc + "logoNameGrupp2.png"}alt="logo1" id="settings-logo" />
+                      </div>
+                      <div className="nav-head-right">
+                          <button className="login-button" id="login-button">Logga in</button>
+                          <button id="options-button">
+                              <img src={imgSrc + "settingGrupp2.png"} alt="logo3" className="permainv" />
+                          </button>
+                      </div>
+                  </div>
+              </div>
+              <div className="navbar-hyperlink">
+                  <ul className="navbar-nav">
+                      <li className="nav-item">
+                          <a id="nav-link-Optional" className="nav-link" href="#">Sport</a>
+                      </li>
+                      <li className="nav-item">
+                          <a id="nav-link-Optional" className="nav-link" href="#">Fritid</a>
+                      </li>
+                      <li className="nav-item">
+                          <a id="nav-link-Optional" className="nav-link" href="#">Kultur</a>
+                      </li>
+                      <li className="nav-item">
+                          <a id="nav-link-Optional" className="nav-link" href="#">Nöje</a>
+                      </li>
+                      <li className="dropdown">
+                          <a href="#" className="drop-link">Väder</a>
+                          <div className="dropdown-content">
+                              <div id="forecast" className="forecast-container"></div>
+                              <div id="gps-info"></div>
+                              <script src="./components/gps.jsx"></script>
+                          </div>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </nav>
+  );
+}
+
+export default navbar;
--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Navbar from './components/navbar/navbar.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <header>
+            <Navbar />
+        </header>
+        
         <App />
   </React.StrictMode>,
 )
