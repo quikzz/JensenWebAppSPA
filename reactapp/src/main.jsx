1d0e29d306ad67dd388daffc9a769a5eed75e6bf
Jool333 <jool333@live.se>
2023-08-21 10:25:03 +0200

    Added leftpanel component

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -4,14 +4,18 @@ import App from './App.jsx'
 import Navbar from './components/navbar/navbar.jsx'
 import Login from './components/loginform/login.jsx'
 import Options from './components/optionsform/options.jsx'
-import './index.css'
 import ScrolltoTop from './components/ScrollToTop/ScrollToTopComponent.jsx'
+import LeftPanel from './components/leftpanel/leftpanel.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <header>
             <Navbar />
         </header>
+        <aside className="main__leftPanel">
+            <LeftPanel/>
+        </aside>
         <ScrolltoTop />
         <App />
         <Login />
