6ce169bb95d6c86a88a5426a14513caf2c68b832
Lagergrenk <k.lagergren@live.se>
2023-08-21 10:21:54 +0200

    ADS
    Ads and tweeks to leftpanel css.

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import Navbar from './components/navbar/navbar.jsx'
 import Login from './components/loginform/login.jsx'
 import Options from './components/optionsform/options.jsx'
+import AdScripts from './components/ads/adScript.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <Navbar />
         </header>
         
+        <AdScripts />
         <App />
         <Login />
         <Options/>
