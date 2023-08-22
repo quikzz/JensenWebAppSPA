d7af3ceda34935084697c5ab921a57af658964b3
quikz <113088508+quikzz@users.noreply.github.com>
2023-08-22 14:59:47 +0200

    Hampus v1

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Navbar from './components/navbar/navbar.jsx'
-<<<<<<< HEAD
+
 import 'bootstrap/dist/css/bootstrap.css'
-=======
+
 import Login from './components/loginform/login.jsx'
 import Options from './components/optionsform/options.jsx'
-import './index.css'
->>>>>>> 8323deca4e73acd8d9cfba0323ae3aa83f0513ca
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
