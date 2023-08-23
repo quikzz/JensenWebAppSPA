87a80ff1596e323a302d460aec9df63f45f3abac
Jool333 <jool333@live.se>
2023-08-23 19:54:51 +0200

    Integrated and fixed bugs from the last merge.

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -2,19 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Navbar from './components/navbar/navbar.jsx'
-
 import 'bootstrap/dist/css/bootstrap.css'
-
 import Login from './components/loginform/login.jsx'
 import Options from './components/optionsform/options.jsx'
-<<<<<<< HEAD
-
-
-=======
 import Footer from './components/footer/footer.jsx'
 import Leftpanel from './components/leftpanel/leftpanel.jsx'
-import './index.css'
->>>>>>> 4c70c70f558747b1a55af0b1e42fec02f4013d75
+
+//import './site.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
