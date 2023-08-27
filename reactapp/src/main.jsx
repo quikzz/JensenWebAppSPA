53b93ed881b6e4e23606436b52be8ac93d5b3ad1
Lagergrenk <k.lagergren@live.se>
2023-08-27 12:31:57 +0200

    Update main.jsx

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import Navbar from './components/navbar/navbar.jsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import Login from './components/loginform/login.jsx'
-import Options from './components/optionsform/options.jsx'
 import Footer from './components/footer/footer.jsx'
 import Leftpanel from './components/leftpanel/leftpanel.jsx'
 import ScrollToTopButton from './components/ScrollToTop/ScrollToTopButton.jsx'
@@ -28,7 +27,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <Footer />
         </footer>
         <Login />
-        <Options />
         <ScrollToTopButton />
   </React.StrictMode>,
 )
