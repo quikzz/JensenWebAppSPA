6c6cb2c5aed41bea2deb9b51bc5a264fabd96e97
Lagergrenk <k.lagergren@live.se>
2023-08-27 15:44:39 +0200

    Grid update. please check and fix if needed

--- reactapp/src/components/navbar/navbar.jsx
+++ reactapp/src/components/navbar/navbar.jsx
@@ -2,12 +2,12 @@
 import './navbar.css';
 import Weather from '@/components/weather/weather.jsx';
 import Settings from '@/components/optionsform/offcanvas.jsx';
+import Navbar from 'react-bootstrap/Navbar';
 
 
 var imgSrc = "./src/assets/images/";
 function navbar() {
     return (
-        <div className="d-flex justify-content-center mt-5">
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
@@ -38,63 +38,10 @@ function navbar() {
                     </div>
                 </div>
             </nav>
-        </div>
+
 
     );
 }
-/*
-function navbar() {
-  return (
-      <nav classNameName="navbar ">
-          <div classNameName="container-navbar">
-              <div classNameName="navbar-header">
-                  <div classNameName="header-side-items">
-                      <div classNameName="nav-head-left">
-                          <div classNameName="datetime" id="datetime">{new Date().toLocaleDateString("en-GB")}</div>
-                          <div id="weather" classNameName="weather-button-container">
-                              <button id="weatherButton">
-                                  <img src={imgSrc + "weather/clear_day.svg"} alt="WeatherImg not found" id="condition" />
-                                  <div id="temperature">Temp:</div>
-                              </button>
-                          </div>
-                      </div>
-                      <div classNameName="nav-head-center">
-                          <img src={imgSrc + "logoNameGrupp2.png"}alt="logo1" id="settings-logo" />
-                      </div>
-                      <div classNameName="nav-head-right">
-                          <Settings />
-                      </div>
-                  </div>
-              </div>
-              <div classNameName="navbar-hyperlink">
-                  <ul classNameName="navbar-nav">
-                      <li classNameName="nav-item">
-                          <a id="nav-link-Optional" classNameName="nav-link" href="#">Sport</a>
-                      </li>
-                      <li classNameName="nav-item">
-                          <a id="nav-link-Optional" classNameName="nav-link" href="#">Fritid</a>
-                      </li>
-                      <li classNameName="nav-item">
-                          <a id="nav-link-Optional" classNameName="nav-link" href="#">Kultur</a>
-                      </li>
-                      <li classNameName="nav-item">
-                          <a id="nav-link-Optional" classNameName="nav-link" href="#">Nöje</a>
-                      </li>
-                      <li classNameName="dropdown">
-                          <a href="#" classNameName="drop-link">Väder</a>
-                          <div classNameName="dropdown-content">
-                              <div id="forecast" classNameName="forecast-container"></div>
-                              <div id="gps-info"></div>
-                              <script src="./components/gps.jsx"></script>
-                          </div>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </nav>
-  );
-}*/
-
 
 export default navbar;
 
--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -8,24 +8,13 @@ import Footer from './components/footer/footer.jsx'
 import Leftpanel from './components/leftpanel/leftpanel.jsx'
 import ScrollToTopButton from './components/ScrollToTop/ScrollToTopButton.jsx'
 
+import Grid from '@/components/grid/grid.jsx'
+
 //import './site.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <header>
-            <Navbar />
-        </header>
-        <main>
-            <aside className="main__leftPanel">
-                <Leftpanel />
-            </aside>
-            <section className="main__top-feed">
-                <App />
-            </section>
-        </main>
-        <footer>
-            <Footer />
-        </footer>
+        <Grid />
         <ScrollToTopButton />
   </React.StrictMode>,
 )
