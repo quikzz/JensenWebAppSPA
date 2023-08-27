ffb14d6178e3bab43fd6a37cf4d2136eaae89948
Jool333 <jool333@live.se>
2023-08-27 12:18:35 +0200

    Added Hampus bootstraped navbar

--- reactapp/src/components/navbar/navbar.jsx
+++ reactapp/src/components/navbar/navbar.jsx
@@ -6,46 +6,83 @@ import Settings from '@/components/optionsform/offcanvas.jsx';
 
 var imgSrc = "./src/assets/images/";
 function navbar() {
+    return (
+        <div className="d-flex justify-content-center mt-5">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item col-md ">
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link fs-3 me-4" href="#">Sport</a>
+                            </li>
+                            <li className="nav-item fs-3 me-4">
+                                <a className="nav-link" href="#">N&#246;je</a>
+                            </li>
+                            <li className="nav-item fs-3 me-4">
+                                <a className="nav-link" href="#">Politik</a>
+                            </li>
+                            <li className="nav-item fs-3 me-5">
+                                <a className="nav-link" href="#">Kultur</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex mx-auto p-2 " role="search">
+                            <input className="form-control  " type="search" placeholder="S&#246;k" aria-label="Search"></input>
+                            <button className="btn btn-outline-success" type="submit">S&#246;k</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+    );
+}
+/*
+function navbar() {
   return (
-      <nav className="navbar ">
-          <div className="container-navbar">
-              <div className="navbar-header">
-                  <div className="header-side-items">
-                      <div className="nav-head-left">
-                          <div className="datetime" id="datetime">{new Date().toLocaleDateString("en-GB")}</div>
-                          <div id="weather" className="weather-button-container">
+      <nav classNameName="navbar ">
+          <div classNameName="container-navbar">
+              <div classNameName="navbar-header">
+                  <div classNameName="header-side-items">
+                      <div classNameName="nav-head-left">
+                          <div classNameName="datetime" id="datetime">{new Date().toLocaleDateString("en-GB")}</div>
+                          <div id="weather" classNameName="weather-button-container">
                               <button id="weatherButton">
                                   <img src={imgSrc + "weather/clear_day.svg"} alt="WeatherImg not found" id="condition" />
                                   <div id="temperature">Temp:</div>
                               </button>
                           </div>
                       </div>
-                      <div className="nav-head-center">
+                      <div classNameName="nav-head-center">
                           <img src={imgSrc + "logoNameGrupp2.png"}alt="logo1" id="settings-logo" />
                       </div>
-                      <div className="nav-head-right">
+                      <div classNameName="nav-head-right">
                           <Settings />
                       </div>
                   </div>
               </div>
-              <div className="navbar-hyperlink">
-                  <ul className="navbar-nav">
-                      <li className="nav-item">
-                          <a id="nav-link-Optional" className="nav-link" href="#">Sport</a>
+              <div classNameName="navbar-hyperlink">
+                  <ul classNameName="navbar-nav">
+                      <li classNameName="nav-item">
+                          <a id="nav-link-Optional" classNameName="nav-link" href="#">Sport</a>
                       </li>
-                      <li className="nav-item">
-                          <a id="nav-link-Optional" className="nav-link" href="#">Fritid</a>
+                      <li classNameName="nav-item">
+                          <a id="nav-link-Optional" classNameName="nav-link" href="#">Fritid</a>
                       </li>
-                      <li className="nav-item">
-                          <a id="nav-link-Optional" className="nav-link" href="#">Kultur</a>
+                      <li classNameName="nav-item">
+                          <a id="nav-link-Optional" classNameName="nav-link" href="#">Kultur</a>
                       </li>
-                      <li className="nav-item">
-                          <a id="nav-link-Optional" className="nav-link" href="#">Nöje</a>
+                      <li classNameName="nav-item">
+                          <a id="nav-link-Optional" classNameName="nav-link" href="#">Nöje</a>
                       </li>
-                      <li className="dropdown">
-                          <a href="#" className="drop-link">Väder</a>
-                          <div className="dropdown-content">
-                              <div id="forecast" className="forecast-container"></div>
+                      <li classNameName="dropdown">
+                          <a href="#" classNameName="drop-link">Väder</a>
+                          <div classNameName="dropdown-content">
+                              <div id="forecast" classNameName="forecast-container"></div>
                               <div id="gps-info"></div>
                               <script src="./components/gps.jsx"></script>
                           </div>
@@ -55,7 +92,7 @@ function navbar() {
           </div>
       </nav>
   );
-}
+}*/
 
 
 export default navbar;
