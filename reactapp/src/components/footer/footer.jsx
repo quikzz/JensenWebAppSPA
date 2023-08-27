3c8314f798efcbbb0656fe1821411548ec3ee38f
quikz <113088508+quikzz@users.noreply.github.com>
2023-08-27 19:05:12 +0200

    Footer (bootstrap)
    Footer remake with bootstrap

--- reactapp/src/components/footer/footer.jsx
+++ reactapp/src/components/footer/footer.jsx
@@ -1,43 +1,37 @@
 import React from 'react';
-import './footer.css';
 
 function Footer() {
     return (
         <div>
-            <div className="footer-grid">
-                <section className="footer">
-                    <ul>
-                        <li>
-                            <h3>Kontakta oss</h3>
-                        </li>
-                        <li><a href="#">Kundservice</a></li>
-                        <li><a href="#">Karriär</a></li>
-                        <li><a href="#">Investerare</a></li>
-                    </ul>
-                </section>
-                <section className="footer">
-                    <ul>
-                        <li>
-                            <h3>Information</h3>
-                        </li>
-                        <li><a href="#">Om oss</a></li>
-                        <li><a href="#">Personuppgiftspolicy</a></li>
-                        <li><a href="#">Allmänna villkor</a></li>
-                    </ul>
-                </section>
-                <section className="footer">
-                    <ul>
-                        <li>
-                            <h3>Tipsa oss</h3>
-                        </li>
-                        <li><a href="#">0701234567</a></li>
-                        <li><a href="#">1234@händelsehorisonten.se</a></li>
-                    </ul>
-                </section>
+            <div className="container">
+                <div className="row">
+                    <section className="col-md-4 footer">
+                        <h3>Kontakta oss</h3>
+                        <ul className="list-unstyled">
+                            <li><a href="#">Kundservice</a></li>
+                            <li><a href="#">Karriär</a></li>
+                            <li><a href="#">Investerare</a></li>
+                        </ul>
+                    </section>
+                    <section className="col-md-4 footer">
+                        <h3>Information</h3>
+                        <ul className="list-unstyled">
+                            <li><a href="#">Om oss</a></li>
+                            <li><a href="#">Personuppgiftspolicy</a></li>
+                            <li><a href="#">Allmänna villkor</a></li>
+                        </ul>
+                    </section>
+                    <section className="col-md-4 footer">
+                        <h3>Tipsa oss</h3>
+                        <ul className="list-unstyled">
+                            <li><a href="#">0701234567</a></li>
+                            <li><a href="#">1234@händelsehorisonten.se</a></li>
+                        </ul>
+                    </section>
+                </div>
             </div>
-            <div className="copyright-footer">
-                &copy; Copyright 2023 - Grupp 2 Jensen
-                
+            <div className="container text-center mt-3">
+                <p>&copy; Copyright 2023 - Grupp 2 Jensen</p>
             </div>
         </div>
     );
