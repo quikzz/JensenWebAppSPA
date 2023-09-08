bd69f797f654c47d272860fef5e9e6b1dd6e7be4
Arshia257 <hosseiniarshia5@gmail.com>
2023-09-08 10:34:40 +0200

    Ändrad knapparna till en drop down

--- reactapp/src/components/navbar/navbar.jsx
+++ reactapp/src/components/navbar/navbar.jsx
@@ -2,17 +2,22 @@
 import './navbar.css';
 import Weather from '@/components/weather/weather.jsx';
 import Settings from '@/components/optionsform/offcanvas.jsx';
-import { useDispatch } from 'react-redux';
-import { setTopic } from '@/redux/topicsortslice';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTopic, selectedTopic } from '@/redux/topicsortslice';
 //import Navbar from 'react-bootstrap/Navbar';
 
 var imgSrc = "./src/assets/images/";
 function navbar() {
+    const searchTopic = useSelector(selectedTopic);
     const dispatch = useDispatch();
+    const topics = ["All", "Politik", "Utbildning", "Religion", "Miljo", "Ekonomi", "LivsstilFritt", "SamhalleKonflikter", "Halsa", "Idrott", "VetenskapTeknik"]
 
-    const handleTopicChange = (topic) => {
-        dispatch(setTopic(topic));
-    }
+    const handleTopicChange = (e) => {
+        const selectedTopic = e.target.value;
+        dispatch(setTopic(selectedTopic));
+    };
+
+
 
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -32,18 +37,12 @@ function navbar() {
                     <ul className="navbar-nav me-1 mb-2 mb-lg-0">
                         <li className="nav-item col-md ">
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link fs-3 me-4" href="#" onClick={() => handleTopicChange("SamhalleKonflikter")}>SamhalleKonflikter</a>
-                        </li>
-                        <li className="nav-item fs-3 me-4">
-                            <a className="nav-link"  href="#" onClick={() => handleTopicChange("Idrott")}>Idrott</a>
-                        </li>
-                        <li className="nav-item fs-3 me-4">
-                            <a className="nav-link"  href="#" onClick={() => handleTopicChange("Ekonomi")}>Ekonomi</a>
-                        </li>
-                        <li className="nav-item fs-3 me-4">
-                            <a className="nav-link" href="#" onClick={() => handleTopicChange("Kultur")}>Kultur</a>
-                        </li>
+                        <label htmlFor="topic">Topics</label>
+                        <select id="topic" name="topic" value={searchTopic} onChange={handleTopicChange}>
+                            {
+                                topics.sort().map((t, i) => <option value={t} key={i}>{t}</option>)
+                            }
+                        </select>
                     </ul>
                     <form className="d-flex ms-3 p-2 " role="search">
                         <input className="form-control  " type="search" placeholder="Sök" aria-label="Search"></input>
